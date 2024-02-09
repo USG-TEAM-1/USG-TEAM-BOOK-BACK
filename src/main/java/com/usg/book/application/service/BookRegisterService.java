8a25b7df55a1f35edb48b354a779ac0a5e8c305f
Choi jun hyoek <chlwnsgur1214@naver.com>
2024-02-09 19:01:07 +0900

    Feat: 책 등록 UseCase 인터페이스 구현체 개발

---
+++ src/main/java/com/usg/book/application/service/BookRegisterService.java
@@ -0,0 +1,47 @@
+package com.usg.book.application.service;
+
+import com.usg.book.application.port.in.BookRegisterCommend;
+import com.usg.book.application.port.in.BookRegisterUseCase;
+import com.usg.book.application.port.out.BookISBNCheckPort;
+import com.usg.book.application.port.out.BookPersistencePort;
+import com.usg.book.domain.Book;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class BookRegisterService implements BookRegisterUseCase {
+
+    private final BookPersistencePort bookPersistencePort;
+    private final BookISBNCheckPort bookISBNCheckPort;
+
+    @Override
+    @Transactional
+    public Long registerBook(BookRegisterCommend commend) {
+
+        // 중앙 도서 API 연동
+        bookISBNCheckPort.bookIsbnCheck(commend.getIsbn());
+
+        // 책 데이터 저장
+        Book book = commendToBook(commend);
+        Long savedBookId = bookPersistencePort.registerBook(book);
+
+        return savedBookId;
+    }
+
+    private Book commendToBook(BookRegisterCommend commend) {
+        return Book
+                .builder()
+                .memberId(commend.getMemberId())
+                .bookName(commend.getBookName())
+                .bookComment(commend.getBookComment())
+                .bookPostName(commend.getBookPostName())
+                .bookPrice(commend.getBookPrice())
+                .isbn(commend.getIsbn())
+                .build();
+    }
+}
