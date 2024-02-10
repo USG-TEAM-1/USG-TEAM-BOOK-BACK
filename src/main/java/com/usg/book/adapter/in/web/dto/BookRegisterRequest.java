dbf9bbba709d454fd7e8ee1f272f28046f532a55
Choi jun hyoek <chlwnsgur1214@naver.com>
2024-02-10 22:39:56 +0900

    Feat: Request DTO 에 이미지 저장을 위한 MultipartFile 추가

--- src/main/java/com/usg/book/adapter/in/web/dto/BookRegisterRequest.java
+++ src/main/java/com/usg/book/adapter/in/web/dto/BookRegisterRequest.java
@@ -1,25 +1,30 @@
 package com.usg.book.adapter.in.web.dto;
 
 import lombok.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.util.ArrayList;
+import java.util.List;
+
+@Setter
 @Getter
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@NoArgsConstructor
 public class BookRegisterRequest {
 
-    private Long memberId; // 어떤 사용자가 책을 등록했는지 알기 위함
     private String bookName; // 책 이름
     private String bookComment; // 책 코멘트
     private String bookPostName; // 책 등록 시 게시글 이름
     private Integer bookPrice; // 책 가격
     private String isbn; // ISBN
+    private List<MultipartFile> images = new ArrayList<>(); // 이미지
 
     @Builder
-    public BookRegisterRequest(Long memberId, String bookName, String bookComment, String bookPostName, Integer bookPrice, String isbn) {
-        this.memberId = memberId;
+    public BookRegisterRequest(String bookName, String bookComment, String bookPostName, Integer bookPrice, String isbn, List<MultipartFile> images) {
         this.bookName = bookName;
         this.bookComment = bookComment;
         this.bookPostName = bookPostName;
         this.bookPrice = bookPrice;
         this.isbn = isbn;
+        this.images = images;
     }
 }
