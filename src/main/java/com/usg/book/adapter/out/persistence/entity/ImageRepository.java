3af27fb719b8fcbab3f10e51359800404dd395b6
Choi jun hyoek <chlwnsgur1214@naver.com>
2024-02-14 20:40:33 +0900

    Feat: 책 PK 로 이미지 조회 쿼리 개발

--- src/main/java/com/usg/book/adapter/out/persistence/entity/ImageRepository.java
+++ src/main/java/com/usg/book/adapter/out/persistence/entity/ImageRepository.java
@@ -1,6 +1,12 @@
 package com.usg.book.adapter.out.persistence.entity;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface ImageRepository extends JpaRepository<ImageEntity, Long> {
+
+    @Query("select i from ImageEntity i" +
+            " where i.bookEntity.id = :bookId")
+    ImageEntity findByBookId(@Param("bookId") Long bookId);
 }
