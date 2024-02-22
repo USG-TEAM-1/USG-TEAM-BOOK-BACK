e1f162e9ccc98176cab32568da2114e751c725b1
Choi jun hyoek <chlwnsgur1214@naver.com>
2024-02-22 17:35:24 +0900

    Feat: Kafka 설정 클래스 환경변수 설정 추가

--- src/main/java/com/usg/book/adapter/in/kafka/config/KafkaConsumerConfig.java
+++ src/main/java/com/usg/book/adapter/in/kafka/config/KafkaConsumerConfig.java
@@ -2,23 +2,30 @@ package com.usg.book.adapter.in.kafka.config;
 
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
-import org.springframework.stereotype.Component;
 
 import java.util.HashMap;
 import java.util.Map;
 
-@Component
+@Configuration
 public class KafkaConsumerConfig {
+
+    @Value("${spring.kafka.consumer.bootstrap-servers}")
+    private String host;
+    @Value("${spring.kafka.consumer.group-id}")
+    private String group;
+
 
     public ConsumerFactory<String, String> factory() {
 
         Map<String, Object> configs = new HashMap<>();
-        configs.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
-        configs.put(ConsumerConfig.GROUP_ID_CONFIG, "book");
+        configs.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, host);
+        configs.put(ConsumerConfig.GROUP_ID_CONFIG, group);
 
         return new DefaultKafkaConsumerFactory<>(
                 configs,
