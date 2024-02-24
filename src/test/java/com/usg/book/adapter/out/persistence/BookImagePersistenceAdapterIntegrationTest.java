a7de4d6fbb8f56a2e28833361d06e2d0326d1770
Choi jun hyoek <chlwnsgur1214@naver.com>
2024-02-24 22:46:14 +0900

    Test: Book PK 로 이미지 엔티티들을 조회하는 테스트 개발

--- src/test/java/com/usg/book/adapter/out/persistence/BookImagePersistenceAdapterIntegrationTest.java
+++ src/test/java/com/usg/book/adapter/out/persistence/BookImagePersistenceAdapterIntegrationTest.java
@@ -3,13 +3,18 @@ package com.usg.book.adapter.out.persistence;
 import com.usg.book.IntegrationExternalApiMockingTestSupporter;
 import com.usg.book.adapter.out.persistence.entity.BookEntity;
 import com.usg.book.adapter.out.persistence.entity.BookRepository;
+import com.usg.book.adapter.out.persistence.entity.ImageEntity;
+import com.usg.book.adapter.out.persistence.entity.ImageRepository;
 import com.usg.book.domain.Image;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
 
 @Transactional
 public class BookImagePersistenceAdapterIntegrationTest extends IntegrationExternalApiMockingTestSupporter {
@@ -18,6 +23,8 @@ public class BookImagePersistenceAdapterIntegrationTest extends IntegrationExter
     private BookImagePersistenceAdapter bookImagePersistenceAdapter;
     @Autowired
     private BookRepository bookRepository;
+    @Autowired
+    private ImageRepository imageRepository;
 
     @Test
     @DisplayName("어뎁터로 이미지를 DB에 저장한다.")
@@ -38,6 +45,41 @@ public class BookImagePersistenceAdapterIntegrationTest extends IntegrationExter
         Long savedBookImageId = bookImagePersistenceAdapter.saveImage(image, savedBookEntity);
 
         // then
-        Assertions.assertThat(savedBookImageId).isNotNull();
+        assertThat(savedBookImageId).isNotNull();
+    }
+
+    @Test
+    @DisplayName("Book PK 로 이미지 엔티티들을 조회한다.")
+    void getImagesUrlsTest() {
+        // given
+        BookEntity savedBookEntity = createBookEntity();
+        ImageEntity imageEntity1 = createImageEntity(savedBookEntity);
+        ImageEntity imageEntity2 = createImageEntity(savedBookEntity);
+        ImageEntity imageEntity3 = createImageEntity(savedBookEntity);
+
+        // when
+        List<ImageEntity> imageEntities = bookImagePersistenceAdapter.getImagesByBookId(savedBookEntity.getId());
+
+        // then
+        assertThat(imageEntities).hasSize(3)
+                .extracting("bookEntity")
+                .contains(savedBookEntity, savedBookEntity, savedBookEntity);
+    }
+
+    private BookEntity createBookEntity() {
+        BookEntity bookEntity = BookEntity.builder().build();
+        return bookRepository.save(bookEntity);
+    }
+
+    private ImageEntity createImageEntity(BookEntity bookEntity) {
+        MockMultipartFile imageFile
+                = new MockMultipartFile("image", "image.jpg", "image/jpeg", new byte[10]);
+        ImageEntity imageEntity = ImageEntity.builder()
+                .uploadFilename(imageFile.getOriginalFilename())
+                .storeFilename("storeFilename")
+                .gcsUrl("gcsUrl")
+                .bookEntity(bookEntity)
+                .build();
+        return imageRepository.save(imageEntity);
     }
 }
