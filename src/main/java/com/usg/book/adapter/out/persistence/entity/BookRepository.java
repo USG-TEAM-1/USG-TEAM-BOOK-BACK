a3583add70e76db964dc167ded4f6aac842737e7
Choi jun hyoek <chlwnsgur1214@naver.com>
2024-02-25 20:37:20 +0900

    Feat: 변경감지 대신 사용할 책 update 쿼리 개발

--- src/main/java/com/usg/book/adapter/out/persistence/entity/BookRepository.java
+++ src/main/java/com/usg/book/adapter/out/persistence/entity/BookRepository.java
@@ -1,6 +1,19 @@
 package com.usg.book.adapter.out.persistence.entity;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface BookRepository extends JpaRepository<BookEntity, Long> {
+
+
+    @Modifying
+    @Query("UPDATE BookEntity b" +
+            " SET b.bookPostName = :bookPostName, b.bookComment = :bookComment, b.bookPrice = :bookPrice" +
+            " where b.id = :bookId")
+    int updateBookByBookId(@Param("bookId") Long bookId,
+                           @Param("bookPostName") String bookPostName,
+                           @Param("bookComment") String bookComment,
+                           @Param("bookPrice") Integer bookPrice);
 }
