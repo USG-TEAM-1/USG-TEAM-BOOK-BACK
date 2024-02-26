a1c162ac1f633305fb19c7147192e0aec295aca5
Choi jun hyoek <chlwnsgur1214@naver.com>
2024-02-26 17:20:44 +0900

    Refactor: findAll 메서드를 제외하고 서비스 레이어에서 엔티티 관련 클래스를 참조하지 않도록 리펙토링

--- src/main/java/com/usg/book/application/service/BookService.java
+++ src/main/java/com/usg/book/application/service/BookService.java
@@ -1,9 +1,7 @@
 package com.usg.book.application.service;
 
-import com.usg.book.adapter.in.web.dto.BookAllResponse;
 import com.usg.book.adapter.out.persistence.entity.BookEntity;
 import com.usg.book.adapter.out.persistence.entity.BookRepository;
-import com.usg.book.adapter.out.persistence.entity.ImageEntity;
 import com.usg.book.adapter.out.persistence.entity.ImageRepository;
 import com.usg.book.application.port.in.BookDeleteCommend;
 import com.usg.book.application.port.in.BookDeleteUseCase;
@@ -20,12 +18,9 @@ import com.usg.book.application.port.out.BookImagePersistencePort;
 import com.usg.book.application.port.out.BookPersistencePort;
 import com.usg.book.application.port.out.MemberPersistencePort;
 import com.usg.book.domain.Book;
-import com.usg.book.exception.UnauthorizedAccessException;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-
-import java.util.Optional;
 
 import org.springframework.data.domain.*;
 import org.springframework.stereotype.Service;
@@ -79,23 +74,23 @@ public class BookService implements BookRegisterUseCase, GetBookUseCase, BookUpd
 
     @Override
     public GetBookServiceResponse getBook(Long bookId) {
-        BookEntity findBookEntity = bookPersistencePort.findById(bookId);
+        Book findBook = bookPersistencePort.findBookById(bookId);
         List<String> imageUrls = bookImagePersistencePort.getImageUrls(bookId);
 
-        // kafka 를 이용해 닉네임 알아오기
-        String nickname = memberPersistencePort.getNicknameByEmail(findBookEntity.getEmail());
+        // redis 를 이용해 닉네임 알아오기
+        String nickname = memberPersistencePort.getNicknameByEmail(findBook.getEmail());
 
         return GetBookServiceResponse
                 .builder()
-                .bookName(findBookEntity.getBookName())
-                .bookComment(findBookEntity.getBookComment())
-                .bookPostName(findBookEntity.getBookPostName())
-                .bookPrice(findBookEntity.getBookPrice())
-                .bookRealPrice(findBookEntity.getBookRealPrice())
+                .bookName(findBook.getBookName())
+                .bookComment(findBook.getBookComment())
+                .bookPostName(findBook.getBookPostName())
+                .bookPrice(findBook.getBookPrice())
+                .bookRealPrice(findBook.getBookRealPrice())
                 .nickname(nickname)
                 .imageUrls(imageUrls)
-                .author(findBookEntity.getAuthor())
-                .publisher(findBookEntity.getPublisher())
+                .author(findBook.getAuthor())
+                .publisher(findBook.getPublisher())
                 .build();
 
     }
@@ -107,7 +102,7 @@ public class BookService implements BookRegisterUseCase, GetBookUseCase, BookUpd
 
         Page<BookEntity> bookPages= bookPersistencePort.findAll(PageRequest.of(page,pageLimit, Sort.by(Sort.Direction.DESC,"id")));
         List<BookEntity> books=bookPages.stream().toList();
-        
+
         List<BookAllServiceResponse> bookAllResponseList = new ArrayList<>();
 
         for (BookEntity book:books) {
@@ -129,43 +124,36 @@ public class BookService implements BookRegisterUseCase, GetBookUseCase, BookUpd
             bookAllResponseList.add(bookAllServiceResponse);
         }
         return new PageImpl<>(bookAllResponseList, pageable, bookPages.getTotalElements());
-        
     }
     
     @Override
     @Transactional
     public Long updateBook(BookUpdateCommend bookUpdateCommend) {
-        // BookPersistencePort를 사용하여 책을 찾아 업데이트
-        Optional<BookEntity> optionalExistingBook = Optional.ofNullable(bookPersistencePort.findById(bookUpdateCommend.getBookId()));
-        
-        // Book이 존재하는지 확인하고, 그렇지 않으면 예외        
-        BookEntity existingBook = optionalExistingBook.orElseThrow(() ->
-                new RuntimeException("Book not found with id: " + bookUpdateCommend.getBookId()));
 
-        // // 책의 소유자 이메일과 요청에서 받은 이메일이 일치하는지 확인
-        // if (!existingBook.getEmail().equals(bookUpdateCommend.getEmail())) {
-        //     throw new UnauthorizedAccessException("You are not authorized to update this book.");
-        // }
-        existingBook.updateBook(bookUpdateCommend.getBookPostName(), bookUpdateCommend.getBookComment(), bookUpdateCommend.getBookPrice());                                                 
+        // BookPersistencePort 를 사용하여 책을 찾아 업데이트
+        Book findBook = bookPersistencePort.findBookById(bookUpdateCommend.getBookId());
 
-        return existingBook.getId();
+         // 책의 소유자 이메일과 요청에서 받은 이메일이 일치하는지 확인
+        findBook.checkSelfEmail(bookUpdateCommend.getEmail());
+
+        bookPersistencePort.updateBook(
+                findBook.getBookId(),
+                bookUpdateCommend.getBookPostName(),
+                bookUpdateCommend.getBookComment(),
+                bookUpdateCommend.getBookPrice()
+        );
+
+        return findBook.getBookId();
     }
 
     @Override
     @Transactional
     public void deleteBook(BookDeleteCommend bookDeleteCommend) {
 
-        Optional<BookEntity> optionalExistingBook = Optional.ofNullable(bookPersistencePort.findById(bookDeleteCommend.getBookId()));
-
-        // 책이 존재하는지 확인하고, 그렇지 않으면 예외를 던짐
-        BookEntity existingBook = optionalExistingBook.orElseThrow(() ->
-                new RuntimeException("Book not found with id: " + bookDeleteCommend.getBookId()));
-
-        
-        // if (!existingBook.getEmail().equals(bookDeleteCommend.getEmail())) {
-        //     throw new UnauthorizedAccessException("You are not authorized to delete this book.");
-        // }
+        Book findBook = bookPersistencePort.findBookById(bookDeleteCommend.getBookId());
         // 소유자의 경우에만 삭제 수행
+        findBook.checkSelfEmail(bookDeleteCommend.getEmail());
+
         bookPersistencePort.deleteById(bookDeleteCommend.getBookId());
     }
 }
