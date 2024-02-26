584d5c729e2d5f7d3c0d4877c8caf618ea1c9a70
Choi jun hyoek <chlwnsgur1214@naver.com>
2024-02-26 16:53:44 +0900

    Test: 이미지 파일들과 책 PK 로 이미지들을 저장하는 테스트 개발

---
+++ src/test/java/com/usg/book/application/service/BookImageServiceTest.java
@@ -0,0 +1,80 @@
+package com.usg.book.application.service;
+
+import com.usg.book.IntegrationExternalApiMockingTestSupporter;
+import com.usg.book.application.port.out.BookImagePersistencePort;
+import com.usg.book.application.port.out.BookPersistencePort;
+import com.usg.book.domain.Book;
+import com.usg.book.domain.Image;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doReturn;
+
+public class BookImageServiceTest extends IntegrationExternalApiMockingTestSupporter {
+
+    @Autowired
+    private BookImageService bookImageService;
+    @Autowired
+    private BookPersistencePort bookPersistencePort;
+    @Autowired
+    private BookImagePersistencePort bookImagePersistencePort;
+
+    @Test
+    @DisplayName("이미지 파일들과 책 PK 로 이미지들을 저장한다.")
+    void saveImagesTest() {
+        // given
+        Book book = createBook();
+        Long savedBookId = bookPersistencePort.registerBook(book);
+        List<MultipartFile> imageLists = createImageLists(2);
+
+        // stub
+        doReturn("gcsUrl").when(imageGcsAdapter).uploadImage(any(Image.class));
+
+        // when
+        bookImageService.saveImages(imageLists, savedBookId);
+
+        // then
+        List<Image> findImages = bookImagePersistencePort.getImagesByBookId(savedBookId);
+        assertThat(findImages).hasSize(2)
+                .extracting("gcsUrl", "bookId")
+                .containsExactlyInAnyOrder(
+                        tuple("gcsUrl", savedBookId),
+                        tuple("gcsUrl", savedBookId)
+                );
+    }
+
+    private Book createBook() {
+        return Book.builder()
+                .email("email")
+                .bookName("bookName")
+                .bookRealPrice(30000)
+                .author("author")
+                .publisher("publisher")
+                .bookPostName("bookPostName")
+                .bookComment("bookComment")
+                .bookPrice(28000)
+                .isbn("isbn")
+                .build();
+    }
+
+    private List<MultipartFile> createImageLists(int loop) {
+        List<MultipartFile> imageFiles = new ArrayList<>();
+
+        for (int i = 0; i < loop; i++) {
+            MockMultipartFile imageFile
+                    = new MockMultipartFile("image", "image.jpg", "image/jpeg", new byte[10]);
+
+            imageFiles.add(imageFile);
+        }
+
+        return imageFiles;
+    }
+}
