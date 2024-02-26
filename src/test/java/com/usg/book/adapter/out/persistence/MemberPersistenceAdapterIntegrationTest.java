55fdf6487103c17b5667b84a309b21a1000cf910
Choi jun hyoek <chlwnsgur1214@naver.com>
2024-02-26 15:51:38 +0900

    Test: 회원 이메일로 레디스에서 회원 닉네임을 조회하는 테스트 개발

--- src/test/java/com/usg/book/adapter/out/persistence/MemberPersistenceAdapterIntegrationTest.java
+++ src/test/java/com/usg/book/adapter/out/persistence/MemberPersistenceAdapterIntegrationTest.java
@@ -2,11 +2,12 @@ package com.usg.book.adapter.out.persistence;
 
 import com.usg.book.IntegrationExternalApiMockingTestSupporter;
 import com.usg.book.domain.Member;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
+
+import static org.assertj.core.api.Assertions.*;
 
 public class MemberPersistenceAdapterIntegrationTest extends IntegrationExternalApiMockingTestSupporter {
 
@@ -26,7 +27,21 @@ public class MemberPersistenceAdapterIntegrationTest extends IntegrationExternal
 
         // then
         String nickname = redisTemplate.opsForValue().get("member : " + member.getEmail());
-        Assertions.assertThat(nickname).isEqualTo(member.getNickname());
+        assertThat(nickname).isEqualTo(member.getNickname());
+    }
+
+    @Test
+    @DisplayName("회원 이메일로 닉네임을 조회한다.")
+    void getNicknameByEmailTest() {
+        // given
+        Member member = createMember();
+        memberPersistenceAdapter.saveMember(member);
+
+        // when
+        String nickname = memberPersistenceAdapter.getNicknameByEmail(member.getEmail());
+
+        // then
+        assertThat(nickname).isEqualTo(member.getNickname());
     }
 
     private Member createMember() {
