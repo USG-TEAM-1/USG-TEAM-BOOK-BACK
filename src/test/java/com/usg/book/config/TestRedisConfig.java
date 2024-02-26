cac4ebf589262acf51a9917a92ed3c4296fe455d
Choi jun hyoek <chlwnsgur1214@naver.com>
2024-02-26 15:45:03 +0900

    Test: 테스트 레디스 설정 클래스 개발

--- src/test/java/com/usg/book/config/TestRedisConfig.java
+++ src/test/java/com/usg/book/config/TestRedisConfig.java
@@ -3,15 +3,17 @@ package com.usg.book.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
-import org.springframework.test.context.ActiveProfiles;
 import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 @Configuration
-@ActiveProfiles("test")
-public class MockRedisConfig {
+@Testcontainers
+@Profile("test")
+public class TestRedisConfig {
 
     @Container
     public static GenericContainer<?> redis = new GenericContainer<>("redis:latest").withExposedPorts(6379);
