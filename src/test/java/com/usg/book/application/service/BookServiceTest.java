8457f8154ac178a1cb1cef6da4085542d0958b91
Choi jun hyoek <chlwnsgur1214@naver.com>
2024-02-26 16:27:16 +0900

    Test: 책 PK 를 이용해 책의 상세 정보를 조회하는 서비스 로직 테스트 개발

--- src/test/java/com/usg/book/application/service/BookServiceTest.java
+++ src/test/java/com/usg/book/application/service/BookServiceTest.java
@@ -2,11 +2,17 @@ package com.usg.book.application.service;
 
 import com.usg.book.IntegrationExternalApiMockingTestSupporter;
 import com.usg.book.application.port.in.BookRegisterCommend;
+import com.usg.book.application.port.in.GetBookServiceResponse;
+import com.usg.book.application.port.out.BookImagePersistencePort;
 import com.usg.book.application.port.out.BookPersistencePort;
+import com.usg.book.application.port.out.MemberPersistencePort;
 import com.usg.book.domain.Book;
+import com.usg.book.domain.Image;
+import com.usg.book.domain.Member;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mock.web.MockMultipartFile;
 
 import static org.assertj.core.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.*;
@@ -18,22 +24,16 @@ public class BookServiceTest extends IntegrationExternalApiMockingTestSupporter 
     private BookService bookService;
     @Autowired
     private BookPersistencePort bookPersistencePort;
+    @Autowired
+    private MemberPersistencePort memberPersistencePort;
+    @Autowired
+    private BookImagePersistencePort bookImagePersistencePort;
 
     @Test
     @DisplayName("Commend 객체를 입력받아 책을 등록한다.")
     void registerBookTest() {
         // given
-        BookRegisterCommend commend = BookRegisterCommend.builder()
-                .email("email")
-                .bookName("bookName")
-                .bookRealPrice(30000)
-                .author("author")
-                .publisher("publisher")
-                .bookPostName("bookPostName")
-                .bookComment("bookComment")
-                .bookPrice(28000)
-                .isbn("isbn")
-                .build();
+        BookRegisterCommend commend = bookRegisterCommend();
 
         // stub
         doNothing().when(bookISBNCheckAdapter).bookIsbnCheck(anyString(), anyInt());
@@ -44,5 +44,63 @@ public class BookServiceTest extends IntegrationExternalApiMockingTestSupporter 
         // then
         Book findBook = bookPersistencePort.findBookById(savedBookId);
         assertThat(findBook.getBookId()).isEqualTo(savedBookId);
+    }
+
+    @Test
+    @DisplayName("책 PK 를 이용해 책의 상세 정보를 조회한다.")
+    void getBookTest() {
+        // given
+        Book book = createBook();
+        Long savedBookId = bookPersistencePort.registerBook(book);
+        Image image = createImage();
+        bookImagePersistencePort.saveImage(image, savedBookId);
+        Member member = Member.builder().email("email").nickname("nickname").build();
+        memberPersistencePort.saveMember(member);
+
+        // when
+        GetBookServiceResponse response = bookService.getBook(savedBookId);
+
+        // then
+        assertThat(response.getNickname()).isEqualTo(member.getNickname());
+        assertThat(response.getImageUrls()).hasSize(1);
+    }
+
+    private BookRegisterCommend bookRegisterCommend() {
+        return BookRegisterCommend.builder()
+                .email("email")
+                .bookName("bookName")
+                .bookRealPrice(30000)
+                .author("author")
+                .publisher("publisher")
+                .bookPostName("bookPostName")
+                .bookComment("bookComment")
+                .bookPrice(28000)
+                .isbn("isbn")
+                .build();
+    }
+
+    private Book createBook() {
+        return Book.builder()
+                .email("email")
+                .bookName("bookName")
+                .bookRealPrice(30000)
+                .author("author")
+                .publisher("publisher")
+                .bookPostName("bookPostName")
+                .bookComment("bookComment")
+                .bookPrice(28000)
+                .isbn("isbn")
+                .build();
+    }
+
+    private Image createImage() {
+        MockMultipartFile imageFile
+                = new MockMultipartFile("image", "image.jpg", "image/jpeg", new byte[10]);
+        return Image.builder()
+                .storeFilename("storeFilename")
+                .originalFilename("originalFilename")
+                .gcsUrl("gcsUrl")
+                .image(imageFile)
+                .build();
     }
 }
