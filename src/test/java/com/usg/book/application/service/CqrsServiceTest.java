ea6f62bae7724ce776a2b1a88f6dc0c2c23f52fa
Choi jun hyoek <chlwnsgur1214@naver.com>
2024-02-27 12:57:10 +0900

    Feat: Cqrs 서비스를 이용해 Redis 에 회원 정보를 저장하는 테스트 개발

---
+++ src/test/java/com/usg/book/application/service/CqrsServiceTest.java
@@ -0,0 +1,33 @@
+package com.usg.book.application.service;
+
+import com.usg.book.IntegrationExternalApiMockingTestSupporter;
+import com.usg.book.application.port.in.MemberWriteCommend;
+import com.usg.book.application.port.out.MemberPersistencePort;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class CqrsServiceTest extends IntegrationExternalApiMockingTestSupporter {
+
+    @Autowired
+    private CqrsService cqrsService;
+    @Autowired
+    private MemberPersistencePort memberPersistencePort;
+
+    @Test
+    @DisplayName("Cqrs 서비스를 이용해 Redis 에 회원 정보를 저장한다.")
+    void memberWriteTest() {
+        // given
+        MemberWriteCommend memberWriteCommend = MemberWriteCommend.builder()
+                .email("email").nickname("nickname").build();
+
+        // when
+        cqrsService.memberWrite(memberWriteCommend);
+
+        // then
+        String nickname = memberPersistencePort.getNicknameByEmail(memberWriteCommend.getEmail());
+        assertThat(nickname).isEqualTo(memberWriteCommend.getNickname());
+    }
+}
